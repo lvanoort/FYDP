b57507fa9cd69689b175f509602163c3e708458d
Derek Chow <derekjchow@gmail.com>
2014-01-28 21:33:21 -0500

    Untested serial reads and publishing.

--- ros_packages/arduino_comm/src/arduino_comm.cpp
+++ ros_packages/arduino_comm/src/arduino_comm.cpp
@@ -6,9 +6,12 @@
 #include <termios.h> /* POSIX terminal control definitions */
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <boost/algorithm/string.hpp>
 
 int fd;
 geometry_msgs::Twist cmd;
+ros::Publisher encoder_pub;
+ros::Time last_arduino_message;
 
 // Opens serial port
 // returns -1 if error
@@ -78,6 +81,48 @@ void timerCallback(const ros::TimerEvent& e)
 {
   //TODO: Map control inputs and outputs and fix sending stuff out
   write_commands(cmd.linear.x, cmd.linear.y);
+  
+  //Health check
+  static int ticks = 0;
+  ticks++;
+  if(ticks > 50) {
+    ROS_INFO("ALIVE");
+    ticks = 0;
+
+    if( (ros::Time::now() - last_arduino_message).toSec() > 2)
+      ROS_INFO("Arduino appears to be dead");
+  }
+
+}
+
+void parse_string(std::string s)
+{
+  std::string delimiters("RL");
+  std::vector<std::string> tokens;
+  boost::split(tokens,s,boost::is_any_of(delimiters));
+  
+  if (tokens.size() != 2) {
+    ROS_ERROR("Parsing error");
+    return;
+  }
+  
+  int r;
+  int l;
+  try {
+    r = boost::lexical_cast<int>( tokens[0] );
+    l = boost::lexical_cast<int>( tokens[1] );
+  } catch( boost::bad_lexical_cast const& ) {
+    ROS_ERROR("Parsing error");
+    return;
+  }
+  
+  geometry_msgs::Twist msg;
+  msg.linear.x = r;
+  msg.linear.y = l;
+  
+  encoder_pub.publish(msg);
+
+  last_arduino_message = ros::Time::now();
 }
 
 int main(int argc, char* argv [])
@@ -92,9 +137,38 @@ int main(int argc, char* argv [])
   ROS_INFO("Serial port openned");
   
 	ros::Subscriber pose_sub = n.subscribe("/command", 1, command_callback);
+	encoder_pub = n.advertise<geometry_msgs::Twist>("/odom", 1);
 	ros::Timer timer = n.createTimer(ros::Duration(0.02), timerCallback);
 	
-	ros::spin();
+	last_arduino_message = ros::Time::now();
+	
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+	
+	char buffer[32];
+	int buf_pos = 0;
+	while(ros::ok())
+	{
+	  while(ros::ok()) {
+	    read(fd, buffer+buf_pos, 1);//TODO: non blocking checks and what not
+	    if( buffer[buf_pos] == '\n' || buf_pos == 31) break;
+	    buf_pos++;
+	  }
+	  
+	  if(buf_pos == 31)
+	  {
+	    //TODO: report error, this is odd
+	    buf_pos = 0;
+	  }
+	  else if(buffer[buf_pos] == '\n')
+	  {
+	    std::string s = std::string(buffer, buf_pos + 1);
+	    parse_string(s);
+	    buf_pos = 0;
+	  }
+	  
+  }
   
+  spinner.stop();
   return 0;
 }
