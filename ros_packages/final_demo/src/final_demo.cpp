a02a586a557b21234f8f3cb830731ffaf8991018
Luke Van Oort <johnvanoort@gmail.com>
2014-03-05 20:49:11 -0500

    added some things to some stuff

--- ros_packages/final_demo/src/final_demo.cpp
+++ ros_packages/final_demo/src/final_demo.cpp
@@ -8,25 +8,61 @@
 #include <math.h>
 #include <vector>
 
+class SubscribeThenPublish {
+
+public:
+
+SubscribeThenPublish(ros::NodeHandle &n) : 
+  n_(n)
+{
+    scan_sub = n_.subscribe("/scan", 1, &SubscribeThePublish::scanCallback, this);
+    velocity_publisher = n_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
+    //map_publisher = n.advertise<nav_msgs::OccupancyGrid>("/map", 1);
+    odom_sub = n_.subscribe("/odom", 1,  &SubscribeThePublish::odomCallback, this);
+}
+
+
+
+void odomCallback(const nav_msgs::Odometry msg){
+
+}
+
 void scanCallback(const sensor_msgs::LaserScan& scan)
 {
 
   #pragma omp parallel for
+  bool obstructed = false;
   for(unsigned int i = 0; i < scan.ranges.size(); i+=20) { // for each ray
   
     if(isnan(scan.ranges[i]) || scan.ranges[i] > scan.range_max || scan.ranges[i] < scan.range_min)
       continue;
 
-    double t  = theta + scan.angle_min + (i*scan.angle_increment);
+    double t  = scan.angle_min + (i*scan.angle_increment);
     double px = x + scan.ranges[i]*cos(t);
     double py = y + scan.ranges[i]*sin(t);
-
+    obstructed = (abs(px) < 0.5 && abs(py) < 2.0);
     
 
   }
-
+  
+  geometry_msgs::Twist cmd;
+  if(obstructed) {
+    cmd.linear.x  = 0.0;
+    cmd.angular.z = 0.6 * 3.14 / 180.0 * 1.0;
+  }
+  ROS_INFO("clear");
+  velocity_publisher.publish(cmd);
 }
 
+protected:
+   ros::NodeHandle n_;
+   ros::Subscriber odom_sub;
+   ros::Subscriber scan_sub;
+   ros::Publisher velocity_publisher;
+
+   //ros::Publisher map_publisher;
+
+}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
  
@@ -36,16 +72,10 @@ int main(int argc, char **argv)
     //Initialize the ROS framework
     ros::init(argc,argv,"main_control");
     ros::NodeHandle n;
- 
-    ros::Subscriber scan_sub = n.subscribe("/scan", 1, scanCallback);
-    ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
-    map_publisher = n.advertise<nav_msgs::OccupancyGrid>("/map", 1);
-    ros::Subscriber odom_sub = n.subscribe("/odom", 1, odomCallback);
+    SubscribeThenPublish(n);
     
     //Set the loop rate
     ros::Rate loop_rate(20);    //20Hz update rate
-
     ros::spin();
-
     return 0;
 }
