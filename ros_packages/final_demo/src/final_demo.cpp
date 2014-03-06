1f5cb1b98dd7a58f0e241088b2a53fc4167a09bf
derek <derek@robot.(none)>
2014-03-05 20:45:02 -0500

    added obstacle detection

--- ros_packages/final_demo/src/final_demo.cpp
+++ ros_packages/final_demo/src/final_demo.cpp
@@ -8,6 +8,8 @@
 #include <math.h>
 #include <vector>
 
+#define OBS_THRESHOLD 5
+
 class SubscribeThenPublish {
 
 public:
@@ -15,10 +17,10 @@ public:
 SubscribeThenPublish(ros::NodeHandle &n) : 
   n_(n)
 {
-    scan_sub = n_.subscribe("/scan", 1, &SubscribeThePublish::scanCallback, this);
-    velocity_publisher = n_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
+    scan_sub = n_.subscribe("/scan", 1, &SubscribeThenPublish::scanCallback, this);
+    velocity_publisher = n_.advertise<geometry_msgs::Twist>("/command", 1);
     //map_publisher = n.advertise<nav_msgs::OccupancyGrid>("/map", 1);
-    odom_sub = n_.subscribe("/odom", 1,  &SubscribeThePublish::odomCallback, this);
+    odom_sub = n_.subscribe("/odom", 1,  &SubscribeThenPublish::odomCallback, this);
 }
 
 
@@ -30,27 +32,33 @@ void odomCallback(const nav_msgs::Odometry msg){
 void scanCallback(const sensor_msgs::LaserScan& scan)
 {
 
-  #pragma omp parallel for
-  bool obstructed = false;
-  for(unsigned int i = 0; i < scan.ranges.size(); i+=20) { // for each ray
+  int obstructed = 0;
+
+  //#pragma omp parallel for
+  //for(unsigned int i = 0; i < scan.ranges.size(); i+=5) { // for each ray
+  for(unsigned int i = 100; i < 540; i+=5) { // for each ray
   
     if(isnan(scan.ranges[i]) || scan.ranges[i] > scan.range_max || scan.ranges[i] < scan.range_min)
       continue;
 
     double t  = scan.angle_min + (i*scan.angle_increment);
-    double px = x + scan.ranges[i]*cos(t);
-    double py = y + scan.ranges[i]*sin(t);
-    obstructed = (abs(px) < 0.5 && abs(py) < 2.0);
-    
+    double px = 0 + scan.ranges[i]*cos(t);
+    double py = 0 + scan.ranges[i]*sin(t);
+    obstructed += (abs(py) < 0.2 && abs(px) < 1.5) ? 1:0;
+    //ROS_INFO("ANGLE IS %f", t);
+  }
 
-  }
-  
   geometry_msgs::Twist cmd;
-  if(obstructed) {
+  if(obstructed > OBS_THRESHOLD) {
+    ROS_INFO("OBSTRUCTED: %d", obstructed);
     cmd.linear.x  = 0.0;
-    cmd.angular.z = 0.6 * 3.14 / 180.0 * 1.0;
+    cmd.angular.z = 0; //0.6 * 3.14 / 180.0 * 1.0;
   }
-  ROS_INFO("clear");
+  else {
+    ROS_INFO("CLEAR: %d", obstructed);
+    cmd.linear.x  = 0.4;
+    cmd.angular.z = 0; //0.6 * 3.14 / 180.0 * 1.0;
+  }
   velocity_publisher.publish(cmd);
 }
 
@@ -62,7 +70,7 @@ protected:
 
    //ros::Publisher map_publisher;
 
-}
+};
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
  
@@ -71,8 +79,8 @@ int main(int argc, char **argv)
 
     //Initialize the ROS framework
     ros::init(argc,argv,"main_control");
-    ros::NodeHandle n;
-    SubscribeThenPublish(n);
+    ros::NodeHandle nh;
+    SubscribeThenPublish stp(nh);
     
     //Set the loop rate
     ros::Rate loop_rate(20);    //20Hz update rate
